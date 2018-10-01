2f18a41ac0a25961898f03d7c8e838e2ba45d40e
Ricky Rojas <rickyrojas19@gmail.com>
2018-10-01 17:06:49 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+  let shoutedString = string.toUpperCase();
+  return shoutedString
+}
+shout('hello');
+
+
+function whisper(string) {
+  let whisperedString = string.toLowerCase();
+  return whisperedString
+}
+whisper('HELLO');
+
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+logShout('hello');
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+logShout('HELLO');
+
+function sayHiToGrandma(string) {
+  if string === lowerCase;
+  return "I can't hear you!"
+}
+
+
